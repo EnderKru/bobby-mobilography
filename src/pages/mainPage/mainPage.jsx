692e95a9022bca8aff5950247109d724e76f231e
EnderKru <enderpro700@gmail.com>
2024-06-06 08:55:56 +0300

    add timer

--- src/pages/mainPage/mainPage.jsx
+++ src/pages/mainPage/mainPage.jsx
@@ -3,14 +3,16 @@ import { PageTemplate } from '../../components/template/template'
 import { Banner } from '../../components/banner/banner'
 import Conditions from '../../components/conditions/Conditions'
 import { Cola } from '../../components/cola/cola'
+import { Timer } from '../../components/timer/timer.jsx'
 
 export default function MainPage(){
     return(
         <div className="main">
             <PageTemplate>
                 <Banner/>
-                <Conditions/>
-                {/* <Cola/> */}
+                <Timer/>
+                <Cola/>
+                {/* <Conditions/> */}
             </PageTemplate>
         </div>
     )
