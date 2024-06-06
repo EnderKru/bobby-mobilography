a7825576b15b3bbfa17d963bceb5b2f00feb7418
VIndsudefe <otasmatov@gmail.com>
2024-06-06 13:00:25 +0600

    response for partners

--- src/pages/mainPage/mainPage.jsx
+++ src/pages/mainPage/mainPage.jsx
@@ -10,7 +10,7 @@ export default function MainPage(){
             <PageTemplate>
                 <Banner/>
                 <Conditions/>
-
+                <Partners/>
             </PageTemplate>
         </div>
     )
