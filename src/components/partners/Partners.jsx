1446c34beeaf3c923d4be7567597795ceb3ea665
EnderKru <enderpro700@gmail.com>
2024-06-07 08:40:24 +0300

    conditions is a little changed

--- src/components/partners/Partners.jsx
+++ src/components/partners/Partners.jsx
@@ -31,8 +31,8 @@ function Partners(props) {
                     <div className={styles.partner}><img src={bobby} alt="logo"/></div>
                     <div className={styles.partner}><img src={kia} alt="logo"/></div>
                     <div className={styles.partner}><img src={coca} alt="logo"/></div>
-                    <div className={styles.partner}><img src={lalafo} alt="logo"/></div>
                     <div className={styles.partner}><img src={samsa} alt="logo"/></div>
+                    <div className={styles.partner}><img src={lalafo} alt="logo"/></div>
                 </div>
             </div>
         </div>
